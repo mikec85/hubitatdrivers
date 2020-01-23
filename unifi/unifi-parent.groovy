3a0c0e90084e3b8f0d8fdc6f92df2594ce231f92
mikec85 <mikec85@users.noreply.github.com>
2020-01-22 18:39:29 -0600

    Update unifi-parent.groovy

--- unifi/unifi-parent.groovy
+++ unifi/unifi-parent.groovy
@@ -367,7 +367,7 @@ def BlockDevice(String mac) {
 		uri:  wxURI2,
         ignoreSSLIssues:  true,
         headers: [ 
-                   Host: "192.168.1.188:${url_port}",
+                   Host: "${ip_addr}:${url_port}",
                    
                    Accept: "*/*",
                    Cookie: "${settings.cookie}"
